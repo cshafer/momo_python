db534bc4d5d38418c2e40cafe351021fd2e4ef18
cshafer <cashafer201@gmail.com>
2024-02-09 12:58:38 -0500

    First commit - testing commit from VScode

---
+++ main.py
@@ -0,0 +1,16 @@
+#main.py
+
+# Choose between idealized experiment or real world experiment.
+# idealized == 0 ; realworld == 1;
+idealized_or_realworld = 0
+
+if idealized_or_realworld == 0:
+    # Here run the idealized experiment
+    test = 0
+    # Choose specific values for the parameters that we need or update parameters within text file
+else:
+    # Here run the real world experiment
+    test = 1
+
+
+# Then here the rest of the code will run as expected
