be68c28ec33b6f73e419fb447e82c7afa3def20d
cshafer <cashafer201@gmail.com>
2024-03-14 16:28:28 -0400

    Generalize create_gaussian_perturbation function by including angle of rotation

--- create_gaussian_perturbation.py
+++ create_gaussian_perturbation.py
@@ -1,10 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-def create_gaussian_perturbation(A, sigma_x, sigma_y, x, y):
+def create_gaussian_perturbation(A, sigma_x, sigma_y, theta, x, y):
     
-    # Create the 2d gaussian grid for the bedrock
-    gaussian_perturbation = A * np.exp(-((np.power(x,2)/(2*sigma_x**2)) + (np.power(y,2)/(2*sigma_y**2))))
+    '''
+    A:  Amplitude
+    sigma_x: width of gaussian distribution in x direction
+    sigma_y: width of gaussian distribution in y direction
+    theta: rotation of distribution in counter-clockwise direction, 0 is no rotation
+    x: x array mesh
+    y: y array mesh
+
+    '''
+
+    # Define the coefficients for a general 2d gaussian distribution
+    a = (np.cos(theta)**2/2*sigma_x**2) + (np.sin(theta)**2/2*sigma_y**2)
+    b = (-np.sin(theta)*np.cos(theta)/2*sigma_x**2) + (np.sin(theta)*np.cos(theta)/2*sigma_y**2)
+    c = (np.sin(theta)**2/2*sigma_x**2) + (np.cos(theta)**2/2*sigma_y**2)
+
+    # Calculate the gaussian distribution
+    gaussian_perturbation = A * np.exp(-(a*(np.power(x,2)) + 2*b*(x*y) + c*(np.power(y,2))))
     return gaussian_perturbation
 
 
